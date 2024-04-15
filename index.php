ea95e624223bcf14d7dace597e771df88d74d934
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-15 16:38:27 -0400

    Add null test

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-
-$one = [1, 12, 123, 'asdfasdf', 0.019];
-$two = 1_000_000
+error_reporting(0);
 
 ?>
 
 <html>
 
 <body>
-    <p><?= var_dump($two) ?></p>
+    <p>Value: <?= $x ?></p>
+    <p>Type: <?= var_dump($x) ?></p>
+    <p>Test: <?= var_dump(is_null($x)) ?></p>
 </body>
 
 </html>
