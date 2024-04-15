de11054a6ba44282860723a17ccde5724e607802
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-15 17:22:05 -0400

    Add null operator

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ error_reporting(0);
 
 $x = 2;
 $x **= 3;
-$target = $x;
+$target = null ?? '1';
 
 ?>
 
 <html>
 
 <body>
-    <p>Value: <?= $target <=> 8 ?></p>
+    <p>Value: <?= $target <=> 0 ?></p>
     <p>Type: <?= var_dump($target) ?></p>
     <p>Null test: <?= var_dump(is_null($target)) ?></p>
     <p>Other: <?= $target ?></p>
