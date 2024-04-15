5d0ab6939aa8666bd869e012b21a2bf1116b3843
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-15 17:02:01 -0400

    Add array tests

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 error_reporting(0);
 
+$array = [];
+$array['one'] = 1;
+array_push($array, 2, 3, 4, 5);
+$x = isset($array[0]);
+array_shift($array);
+
 ?>
 
 <html>
 
 <body>
-    <p>Value: <?= $x ?></p>
-    <p>Type: <?= var_dump($x) ?></p>
-    <p>Test: <?= var_dump(is_null($x)) ?></p>
+    <p>Value: <?= print_r($array) ?></p>
+    <p>Type: <?= var_dump($array) ?></p>
+    <p>Null test: <?= var_dump(is_null($array)) ?></p>
+    <p>Other: <?= count($array) ?></p>
 </body>
 
 </html>
