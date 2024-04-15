46770f9c5695402b184ff63f88ede87cca7a0eab
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-15 17:18:12 -0400

    Add spaceship

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 error_reporting(0);
 
-$x = 10.12 % 3.1231;
+$x = 2;
+$x **= 3;
+$target = $x;
 
 ?>
 
 <html>
 
 <body>
-    <p>Value: <?= $x ?></p>
-    <p>Type: <?= var_dump($x) ?></p>
-    <p>Null test: <?= var_dump(is_null($x)) ?></p>
-    <p>Other: <?= $x ?></p>
+    <p>Value: <?= $target <=> 8 ?></p>
+    <p>Type: <?= var_dump($target) ?></p>
+    <p>Null test: <?= var_dump(is_null($target)) ?></p>
+    <p>Other: <?= $target ?></p>
 </body>
 
 </html>
