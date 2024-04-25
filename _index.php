b798e234a027ac8a4b4c6fec3070ab3b5e9fc2e9
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-25 11:57:12 -0400

    Add array_combine

--- _index.php
+++ _index.php
@@ -3,12 +3,13 @@
 // require_once 'components/test/include_file.php';
 
 $test_array = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
+$test_array2 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
 
 ?>
 
 <div>
     <h1>Index</h1>
     <pre>
-        <?= print_r(array_chunk($test_array, 2, true)) ?>
+        <?= print_r(array_combine($test_array, $test_array2)) ?>
 </pre>
 </div>
