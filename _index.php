d8823ac218b609ae516eda297f2ea8e632a2d87a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-25 11:58:37 -0400

    Add array_filter

--- _index.php
+++ _index.php
@@ -10,6 +10,6 @@ $test_array2 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
 <div>
     <h1>Index</h1>
     <pre>
-        <?= print_r(array_combine($test_array, $test_array2)) ?>
+        <?= print_r(array_filter($test_array, fn ($item) => $item % 2 === 0)) ?>
 </pre>
 </div>
