0c32fea0198a9ee68c57cd426e5ae2018b42d48a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-25 12:09:04 -0400

    Use usort

--- _index.php
+++ _index.php
@@ -2,14 +2,18 @@
 
 // require_once 'components/test/include_file.php';
 
-$test_array = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
-$test_array2 = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4];
+$test_array = ['a' => 100, 'b' => 2315, 'c' => 3345, 'd' => 4542];
+$test_array2 = ['a' => 1123, 'b' => 273546, 'c' => 33645, 'd' => 484];
+
+$a = array_combine($test_array2, $test_array);
+
+usort($a, fn ($a, $b) => $a <=> $b);
 
 ?>
 
 <div>
     <h1>Index</h1>
     <pre>
-        <?= print_r(array_filter($test_array, fn ($item) => $item % 2 === 0)) ?>
+        <?= print_r($a) ?>
 </pre>
 </div>
