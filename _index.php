5c6ef3d952b9fc339c4af2017019cc1466c56afd
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-29 17:49:01 -0400

    Add scandir

--- _index.php
+++ _index.php
@@ -2,18 +2,13 @@
 
 // require_once 'components/test/include_file.php';
 
-$test_array = ['a' => 100, 'b' => 2315, 'c' => 3345, 'd' => 4542];
-$test_array2 = ['a' => 1123, 'b' => 273546, 'c' => 33645, 'd' => 484];
-
-$a = array_combine($test_array2, $test_array);
-
-usort($a, fn ($a, $b) => $a <=> $b);
+$dir = scandir(__DIR__)
 
 ?>
 
 <div>
     <h1>Index</h1>
     <pre>
-        <?= print_r($a) ?>
+        <?= print_r($dir) ?>
 </pre>
 </div>
