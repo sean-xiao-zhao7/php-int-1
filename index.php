a2c723241021271fa6647267eb0632cdb2187995
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-29 17:58:18 -0400

    Add autoload for composer

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-$title = 'Home';
-$child_path = '_index.php';
-require_once 'components/layout/layout.php';
+// $title = 'Home';
+// $child_path = '_index.php';
+// require_once 'components/layout/layout.php';
+
+
+require_once __DIR__ . '/vendor/autoload.php';
