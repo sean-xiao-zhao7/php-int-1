290e93db4a11e812b5191a60576c6fe7c4ab9761
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-01 14:30:05 -0400

    Remove testing of DebtCollectionAmountInvalidException

--- classes/DebitCollectorService.php
+++ classes/DebitCollectorService.php
@@ -8,7 +8,7 @@ class DebitCollectorService
 {
     public function collect(DebtCollectorInterface $collector): string
     {
-        $amount = -10; // mt_rand(100, 1000);
+        $amount = mt_rand(-1000, 1000);
         $result = $collector->collect($amount);
 
         return 'Collected $' . $result . ' out of $' . $amount . PHP_EOL;
