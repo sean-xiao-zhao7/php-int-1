e0a8e5acb98c0b0a379ea11189db5a675679c1d4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-01 15:15:14 -0400

    Use DateTimeTest

--- _index.php
+++ _index.php
@@ -1,13 +1,13 @@
 <?php
 
-
+use Xi\Php\DateTimeTest;
 
 ?>
 
 <div>
     <h1>Sandbox</h1>
     <pre>
-
+        <?= DateTimeTest::exec_test() ?>
     </pre>
 
 </div>
--- classes/DateTimeTest.php
+++ classes/DateTimeTest.php
@@ -4,17 +4,13 @@ namespace Xi\Php;
 
 class DateTimeTest
 {
-    public function __construct()
-    {
-    }
-
-    public static function test()
+    public static function exec_test()
     {
         $period = new \DatePeriod(new \DateTime(), new \DateInterval("P1D"), new \DateTime("next month"));
 
-        $result = '';
+        $result = PHP_EOL;
         foreach ($period as $day) {
-            $result += $day->format('D M/d/Y') . PHP_EOL;
+            $result .= $day->format('D M/d/Y') . PHP_EOL;
         }
 
         return $result;
