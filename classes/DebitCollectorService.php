297d1cd136ab1b13afda476c3e68f95e47680151
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-01 14:18:18 -0400

    Throws InvalidArgumentException in DebtCollector

--- classes/DebitCollectorService.php
+++ classes/DebitCollectorService.php
@@ -8,7 +8,7 @@ class DebitCollectorService
 {
     public function collect(DebtCollectorInterface $collector): string
     {
-        $amount = mt_rand(100, 1000);
+        $amount = -10; // mt_rand(100, 1000);
         $result = $collector->collect($amount);
 
         return 'Collected $' . $result . ' out of $' . $amount . PHP_EOL;
--- classes/DebtCollector.php
+++ classes/DebtCollector.php
@@ -8,6 +8,9 @@ class DebtCollector implements DebtCollectorInterface
 {
     public function collect(float $amount): float
     {
+        if ($amount <= 0.0) {
+            throw new \InvalidArgumentException("Amount must be more than 0.");
+        }
         return mt_rand((int) ($amount * 0.5), (int) $amount);
     }
 }
