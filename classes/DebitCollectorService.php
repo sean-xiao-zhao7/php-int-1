981a3e742280cb8d85bd33050d851fd8e9d260cf
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-01 14:33:19 -0400

    Add catching of DebtCollectionAmountInvalidException

--- classes/DebitCollectorService.php
+++ classes/DebitCollectorService.php
@@ -3,13 +3,19 @@
 namespace Xi\Php;
 
 use Xi\Php\Interfaces\DebtCollectorInterface;
+use Xi\Php\Exceptions\DebtCollectionAmountInvalidException;
 
 class DebitCollectorService
 {
     public function collect(DebtCollectorInterface $collector): string
     {
         $amount = mt_rand(-1000, 1000);
-        $result = $collector->collect($amount);
+
+        try {
+            $result = $collector->collect($amount);
+        } catch (DebtCollectionAmountInvalidException $e) {
+            return 'Amount $' . $amount . ' is invalid.' . PHP_EOL;
+        }
 
         return 'Collected $' . $result . ' out of $' . $amount . PHP_EOL;
     }
