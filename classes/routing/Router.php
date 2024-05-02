07bd11d822213d9b8d8e17b39b6dfa3214a220e4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-05-02 15:09:00 -0400

    Add resolve_route

--- classes/routing/Router.php
+++ classes/routing/Router.php
@@ -8,10 +8,17 @@ class Router
 {
     private $routes = array();
 
-    public function register(string $route, AbstractController $action): self
+    public function register(string $route, AbstractController $controller): self
     {
-        $this->routes[$route] = $action;
+        $this->routes[$route] = $controller;
 
         return $this;
+    }
+
+    public function resolve_route(string $requestUri): void
+    {
+        $uri = explode('?', $requestUri)[0];
+        $controller = $this->routes[$uri];
+        $controller->render_view();
     }
 }
